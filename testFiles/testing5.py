e4f220e841cfe76cc4df51c9fd8ca5aa83b5e9c6
Krishnanshu Agrawal <krishnanshuagrawal051@gmail.com>
2026-08-16 16:28:46 +0530

    pybullet basics: created a multibody with rotating link

--- testFiles/testing5.py
+++ testFiles/testing5.py
@@ -20,13 +20,6 @@ visShapeCube = p.createVisualShape(
     rgbaColor=[1, 0, 0, 1] 
 )
 
-cube = p.createMultiBody(
-    baseMass=1,
-    baseCollisionShapeIndex=colShapeCube,
-    baseVisualShapeIndex=visShapeCube,
-    basePosition=[0, 0, 5]
-)
-
 colShapeSphere= p.createCollisionShape(
     p.GEOM_SPHERE,
     radius=0.5)
@@ -34,36 +27,34 @@ colShapeSphere= p.createCollisionShape(
 visShapeSphere = p.createVisualShape(
     p.GEOM_SPHERE,
     radius=0.5,
-    rgbaColor=[1, 1, 0, 0.4] 
+    rgbaColor=[1, 1, 0, 1] 
 )
 
-Sphere = p.createMultiBody(
+shape = p.createMultiBody(
     baseMass=1,
-    baseCollisionShapeIndex=colShapeSphere,
-    baseVisualShapeIndex=visShapeSphere,
-    basePosition=[0, 1, 2]
-)
-
-colShapeCylinder= p.createCollisionShape(
-    p.GEOM_CYLINDER,
-    radius=0.5,
-    height=1)
-
-visShapeCylinder = p.createVisualShape(
-    p.GEOM_CYLINDER,
-    radius=0.5,
-    length=1,
-    rgbaColor=[1, 0, 0.5, 0.2] 
-)
-
-Cylinder = p.createMultiBody(
-    baseMass=1,
-    baseCollisionShapeIndex=colShapeCylinder,
-    baseVisualShapeIndex=visShapeCylinder,
-    basePosition=[2, 1, 2]
+    baseCollisionShapeIndex=colShapeCube,
+    baseVisualShapeIndex=visShapeCube,
+    basePosition=[0, 0, 2],
+    linkMasses=[1],
+    linkCollisionShapeIndices=[colShapeSphere],
+    linkVisualShapeIndices=[visShapeSphere],
+    linkPositions=[[0, 0, 1]],
+    linkOrientations=[[0, 0, 0, 1]],
+    linkParentIndices=[0],
+    linkInertialFramePositions=[[0, 0, 0]],
+    linkInertialFrameOrientations=[[0, 0, 0, 1]],
+    linkJointTypes=[p.JOINT_REVOLUTE],
+    linkJointAxis=[[0, 1, 1]]
 )
 
 while True:
+    p.setJointMotorControl2(
+    shape,
+    0,
+    p.VELOCITY_CONTROL,
+    targetVelocity=20,
+    force=10
+)
     p.stepSimulation()
     time.sleep(1/240)
 
