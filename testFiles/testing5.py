eb5b1bd5f5d73e8a630db407d652c22c5a82912d
Krishnanshu Agrawal <krishnanshuagrawal051@gmail.com>
2026-08-16 16:12:37 +0530

    pybullet basics: made shapes using api

--- testFiles/testing5.py
+++ testFiles/testing5.py
@@ -8,22 +8,59 @@ p.setGravity(0, 0, -9.81)
 
 planeId = p.loadURDF("plane.urdf")
 
-colShape= p.createCollisionShape(
+halfExtents = [1, 1, 0.5]
+colShapeCube= p.createCollisionShape(
     p.GEOM_BOX,
-    halfExtents=[0.5, 0.5, 0.5]
+    halfExtents=halfExtents
 )
 
-visShape = p.createVisualShape(
+visShapeCube = p.createVisualShape(
     p.GEOM_BOX,
-    halfExtents=[0.5, 0.5, 0.5],
+    halfExtents=halfExtents,
     rgbaColor=[1, 0, 0, 1] 
 )
 
 cube = p.createMultiBody(
-    baseMass=100,
-    baseCollisionShapeIndex=colShape,
-    baseVisualShapeIndex=visShape,
-    basePosition=[0.5, 0, 0.5]
+    baseMass=1,
+    baseCollisionShapeIndex=colShapeCube,
+    baseVisualShapeIndex=visShapeCube,
+    basePosition=[0, 0, 5]
+)
+
+colShapeSphere= p.createCollisionShape(
+    p.GEOM_SPHERE,
+    radius=0.5)
+
+visShapeSphere = p.createVisualShape(
+    p.GEOM_SPHERE,
+    radius=0.5,
+    rgbaColor=[1, 1, 0, 0.4] 
+)
+
+Sphere = p.createMultiBody(
+    baseMass=1,
+    baseCollisionShapeIndex=colShapeSphere,
+    baseVisualShapeIndex=visShapeSphere,
+    basePosition=[0, 1, 2]
+)
+
+colShapeCylinder= p.createCollisionShape(
+    p.GEOM_CYLINDER,
+    radius=0.5,
+    height=1)
+
+visShapeCylinder = p.createVisualShape(
+    p.GEOM_CYLINDER,
+    radius=0.5,
+    length=1,
+    rgbaColor=[1, 0, 0.5, 0.2] 
+)
+
+Cylinder = p.createMultiBody(
+    baseMass=1,
+    baseCollisionShapeIndex=colShapeCylinder,
+    baseVisualShapeIndex=visShapeCylinder,
+    basePosition=[2, 1, 2]
 )
 
 while True:
